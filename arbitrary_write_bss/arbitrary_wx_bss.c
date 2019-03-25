3f6fa73f78182fa68c530b5474a60d822647b377
tripoloski1337 <arsalan.dp@gmail.com>
2019-03-25 14:58:07 +0700

    fixed buffer size and nx protection

---
+++ arbitrary_write_bss/arbitrary_wx_bss.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  char buf[20]; // limitation
+  printf("+-------------------------------------------+\n");
+  printf("|            developed by tripoloski   2019 |\n");
+  printf("|  how to :                                 |\n");
+  printf("| - write on a memory                       |\n");
+  printf("| - Execute the code that you wrote earlier |\n");
+  printf("| - bss can help you !                      |\n");
+  printf("+-------------------------------------------+\n");
+  printf("local exploit > ");gets(&buf);
+}
